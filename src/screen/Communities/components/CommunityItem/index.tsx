7c50e3071ed9ebd5429804c955111b5306b0242e
Mag1strVark <molot2101@gmail.com>
2024-03-08 02:41:17 +0500

    fix: edit page

--- src/screen/Communities/components/CommunityItem/index.tsx
+++ src/screen/Communities/components/CommunityItem/index.tsx
@@ -10,12 +10,19 @@ const CommunityItem = (props: IProps) => {
     <>
       <ListItem>
         <div className="flex-align-start gap-32">
+          <div
+            style={{
+              width: '100px',
+              height: '100px',
+              borderRadius: '100px',
+              border: `2px solid ${props.groups.avatar_color ? props.groups.avatar_color : 'black'}`,
+            }}
+          ></div>
           <div className="flex-column-max-content gap-4">
             <span>{props.groups.name}</span>
-            <span>{props.groups.closed}</span>
-            {props.groups.avatar_color && <span>{props.groups.avatar_color}</span>}
-            <span>{props.groups.members_count}</span>
-            {props.groups.friends && <span>{props.groups.friends.length}</span>}
+            <span>{props.groups.closed ? 'Группа закрытая' : 'Группа открытая'}</span>
+            <span>Кол-во подписчиков: {props.groups.members_count}</span>
+            {props.groups.friends && <span>Друзья</span>}
           </div>
         </div>
       </ListItem>
